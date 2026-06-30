7b28c85ffec9400c5238ce2d17b556ffee2ef673
Christian von Buedingen <karl@insurancemasters.biz>
2026-06-30 16:22:03 -0400

    Configure Vercel serverless routing for Express API endpoint

--- server.js
+++ server.js
@@ -254,6 +254,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
